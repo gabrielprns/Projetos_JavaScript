317d7ee05ff603433e69518499cde0d9ebb3a492
gabrielprns <gabrielpedrornn@outlook.com>
2021-09-06 23:50:30 -0300

    Project

---
+++ Project_1/cond.js
@@ -0,0 +1,16 @@
+let hora = 19
+
+if (hora > 6 && hora < 12){
+  console.log("Bom Dia");
+  alert("Bom Dia");
+}
+
+else if (hora > 12 && hora < 18){
+  console.log("Boa Tarde");
+  alert("Boa Tarde");
+}
+
+else if (hora > 18 && hora < 23){
+  console.log("Boa Noite");
+  alert("Boa Noite");
+}
