da5a7d0baa1a0d8feda349dc1886407a5ce719ae
gabrielprns <gabrielpedrornn@outlook.com>
2021-09-18 23:59:06 -0300

    project

--- Projects/Project_2/max.js
+++ Projects/Project_2/max.js
@@ -2,7 +2,7 @@
 
 let valorMaior = max(14, 3);
 console.log(valorMaior);
-alert(valorMaior);
+//alert(valorMaior);
 
 function max(num1, num2) {
   return num1>num2 ? num1: num2 ;
---
+++ Projects/Project_2/vel.js
@@ -0,0 +1,8 @@
+verificarVelocidade(80);
+
+function verificarVelocidade(velocidade) {
+  if (velocidade <=70)
+    console.log('ok');
+    alert('ok');
+
+}
